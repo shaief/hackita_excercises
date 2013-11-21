892a65bd5713ca8d7fb69184b16d60c6d3ed76d5
shaief <shaief@gmail.com>
2013-11-22 01:54:09 +0200

    fixed solution, skipped numpy

--- basics/ex6.py
+++ basics/ex6.py
@@ -1,8 +1,15 @@
-import numpy as np
-
 print "enter a number: "
 n = raw_input()
 nn = int(n)
+
+print "Here is your multiplication board :) :"
+
+multi_list = [[((i+1)*(j+1)) for i in range(nn)] for j in range(nn)]
+for i in range(nn):
+	print multi_list[i][:]
+
+import numpy as np
+
 multi = np.zeros((nn,nn), dtype=np.int)
 for i in range(nn):
 	for j in range(nn):
