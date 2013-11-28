a5cc25c9b213b1d2dea2597e2898e47e2c4ef6b3
shaief <shaief@gmail.com>
2013-11-28 15:15:33 +0200

    ex5 one liner :)

--- basics/ex5.py
+++ basics/ex5.py
@@ -8,11 +8,11 @@ def palindrom(user_word):
 	return
 
 def palindrom_one_liner(user_word):
-	[x=1 if (user_word[i] <> user_word[-i-1]) for i in range(len(user_word)/2)]
-	print user_word + " is a palindrom!!!"
-	return
+	if user_word == user_word[::-1]:
+		return True
+	return False
 
 print "enter a word: "
 u_word = raw_input()
 
-palindrom_one_liner(u_word)
+print palindrom_one_liner(u_word)
